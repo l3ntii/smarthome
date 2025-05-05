cb7da580d3cb0f99500c19c9ae92aa489fe71a7a
l3ntii <florentsalihi007@gmail.com>
2025-05-05 19:31:35 +0200

    Update main.py

--- project/aktor-rpi/main.py
+++ project/aktor-rpi/main.py
@@ -1,1 +1,26 @@
+import threading
+from sensors import temp, safety, alarm, light
 
+def main():
+    print("Temperature and Safety sensors are running... (Press Ctrl+C to stop)")
+
+    temp_thread = threading.Thread(target=temp.run, daemon=True)
+    temp_thread.start()
+
+    safety_thread = threading.Thread(target=safety.run, daemon=True)
+    safety_thread.start()
+
+    alarm_thread = threading.Thread(target=alarm.run, daemon=True)
+    alarm_thread.start()
+
+    light_thread = threading.Thread(target=light.run, daemon=True)
+    light_thread.start()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("\nShutting down program.")
+
+if __name__ == "__main__":
+    main()
